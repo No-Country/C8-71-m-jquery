6a4a79fc2e7a67fbbbdba2bb4efbccf7c00a2a83
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2022-11-28 16:45:38 -0500

    Creacion de página seleccion de perfil
    Usando html, css y js para simular hover entre perfiles

---
+++ js/seleccionPerfil.js
@@ -0,0 +1,45 @@
+const perfilAnfitrion = document.querySelector(".seleccionPerfil__anfitrion");
+const perfilTurista = document.querySelector(".seleccionPerfil__turista");
+const overlayTurista = document.querySelector(".turista__overlay");
+const overlayAnfitrion = document.querySelector(".anfitrion__overlay");
+
+
+const activarOverlay = (perfil) => perfil.addEventListener('mouseover',
+    () => {
+        switch (perfil) {
+            case perfilTurista:
+
+                overlayAnfitrion.classList.add('active');
+                break;
+
+            case perfilAnfitrion:
+
+                overlayTurista.classList.add('active');
+                break;
+        }
+    }
+);
+
+const quitarOverlay = (perfil) => perfil.addEventListener('mouseout',
+    () => {
+        switch (perfil) {
+            case perfilTurista:
+
+                overlayAnfitrion.classList.remove('active');
+                break;
+
+            case perfilAnfitrion:
+
+                overlayTurista.classList.remove('active');
+                break;
+        }
+    }
+);
+
+activarOverlay(perfilAnfitrion);
+activarOverlay(perfilTurista);
+
+quitarOverlay(perfilAnfitrion);
+quitarOverlay(perfilTurista);
+
+console.log(overlayAnfitrion)
