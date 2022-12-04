58236474022604b8ce358823d960138c2cfe78b3
Arn.28 <arnold.balabarca.27@unsch.edu.pe>
2022-12-04 18:55:19 -0500

    update slider del banner

--- js/sliderBanner.js
+++ js/sliderBanner.js
@@ -1,10 +1,34 @@
-const slider = document.querySelector('.banner__slider');
+const slider = document.querySelector(".banner__slider");
+const imagenes = document.querySelectorAll(".banner__slider .slide");
+const bottom_slider = document.querySelectorAll('.bottom__slider button');
 var primerSlide = null;
 
 const moverSlide = () => {
-    slider.style.transition = 'all 1s ease';
-    slider.style.marginLeft = '-100%';
+  slider.style.transition = "all 1s ease";
+  slider.style.marginLeft = "-100%";
+
+  setTimeout(() => {
+    primerSlide = slider.firstElementChild;
+    slider.appendChild(primerSlide);
+    slider.style.transition = "unset";
+    slider.style.marginLeft = "0%";
+  }, 1000);
 };
+
+const animaSlider = () => {
+  setInterval(moverSlide, 3000);
+};
+
+const slideSelect = () => {
+  for (let i = 0; i < bottom_slider.length; i++) {
+    bottom_slider[i].addEventListener('click', () => {
+      
+    })
+    
+  }
+};
+
+animaSlider();
 
 // (function () {
 //     //Objeto con las propiedades del efecto slider
